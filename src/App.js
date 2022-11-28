497370c074bf5b2d510067c401d5f5861cb7d773
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-11-28 17:45:43 +0000

    Added Navbar component

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import Search from './pages/search/Search';
 import Recipe from './pages/recipe/Recipe';
 
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
+        <Navbar />
         <Switch>
           <Route exact path='/'>
             <Home />
