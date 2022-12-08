b513d3a666cb681955400528fa74041f8d4df5fe
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2022-12-08 15:56:14 +0000

    Styled searchbar component

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,12 +1,16 @@
 import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import './Searchbar.css';
 
 export default function SearchBar() {
   const [term, setTerm] = useState('');
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    history.push(`/search?q=${term}`);
   };
 
   return (
